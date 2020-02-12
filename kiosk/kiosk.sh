b7f4f8defdcc25e0de6ed4fd8201699e26f47450
Julian Oes <julian@oes.ch>
2020-02-12 21:24:03 +0100

    Don't start chromium in background
    Otherwise we exit the script and take chromium with us.

--- kiosk/kiosk.sh
+++ kiosk/kiosk.sh
@@ -9,4 +9,4 @@ unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --check-for-update-interval=31536000 --disable-infobars --kiosk file:///home/pi/index.html &
+/usr/bin/chromium-browser --noerrdialogs --check-for-update-interval=31536000 --disable-infobars --kiosk file:///home/pi/index.html
